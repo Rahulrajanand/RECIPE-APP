c56cf4d33f67e37712b4bbe6ae3d356344f93931
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-08-03 23:35:34 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import SideBar from "./components/SideBar"
+import HomePage from "./pages/HomePage"
 
 function App() {
   
-
   return (
     <>
-      
+      <div className="flex">
+      <SideBar/>
+      <HomePage/>
+      </div>
     </>
   )
 }
