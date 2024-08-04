7ea0d118b7acb96064b16b7bb46420109d47ec03
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-08-04 20:19:18 +0530

    Updated

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -12,8 +12,16 @@ export default SideBar
 
 const DesktopSidebar = () => {
   return (
-    <div className='p-3 md:p-10 border-r min-h-screen w-24 md:w-64'>
+    <div className='p-3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block'>
+      <div className='flex flex-col gap-20 sticky top-10 left-0'>
+        <div className='w-full'>
+          <img src="/logo.svg" alt="" className='block md:hidden'/>
+          <img src="/mobile-logo.svg" alt="logo" className='block md:hidden'/>
+        </div>
+          <ul className='flex flex-col items-center md:items-start gap-8'>
 
+          </ul>
+      </div>
     </div>
   )
 }
