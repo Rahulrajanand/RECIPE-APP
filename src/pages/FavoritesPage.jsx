490bb37a7b26048d8391503bf8640f028d4615c3
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-09-01 23:37:05 +0530

    Favorites added

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
 const FavoritesPage = () => {
+  const fav = false;
+
   return (
-    <div>FavoritesPage</div>
+    <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen">
+      <div className="max-w-screen-lg mx-auto">
+        <p className="font-bold text-3xl md:text-5xl my-4">My Favorites </p>
+
+        {!fav && (
+          <div className="h-[80vh] flex flex-col items-center gap-4">
+                <img src="/404.svg" className="h-3/4" alt="404 svg" />
+          </div>
+        )}
+
+        
+      </div>
+    </div>
   )
 }
 
