0f36695fd004709e25a9d59cad59c03d1281e277
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-09-03 12:01:14 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,33 @@
-
 import { Search } from "lucide-react"
 import RecipeCard from "../components/RecipeCard"
+import { useEffect, useState } from "react"
+
+const APP_ID = "3298bbb0"
+const APP_KEY = "df6a6b2761683fae180f1241d088ece"
 
 const HomePage = () => {
+
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRecipes = async (searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+    try {
+      const res = await fetch(`https://api.edamam.com/api/recipes/v2/?app_id=${APP_ID}&app_key=${APP_KEY}&q=${searchQuery}&type=public`)
+      const data = await res.json();
+      console.log(data);      
+    } catch (error) {
+      console.log(error.message);    
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchRecipes("chicken")
+  }, [])
+
   return <div className="bg-[#faf9fb] p-10 flex-1">
     <div className="max-w-screen-lg mx-auto">
       <form>
